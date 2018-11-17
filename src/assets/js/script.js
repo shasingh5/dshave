0ff80c18698f4bea1e87eaa52ec82f050f88e676
shasingh5 <shashi4singh@gmail.com>
2018-11-17 19:30:27 +0530

    To added slider in banner, slider to our services and slider our work

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $('.hero-slider').slick({
+    $('.barber-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
@@ -11,65 +11,58 @@ $(function() {
             //nextArrow: '.artitsts-wrapper .slider-pager .next'
     });
 
-    $('.slider-for').slick({
-        slidesToShow: 1,
+    $('.work-slider').slick({
+        slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
+        arrows: true,
+        autoplay: true,
+        responsive: [            
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
-    $('.slider-nav').slick({
-        slidesToShow: 4,
+    $('.services-slider').slick({
+        slidesToShow: 3,
         slidesToScroll: 1,
-        asNavFor: '.slider-for',
+        //asNavFor: '.slider-for',
         centerMode: false,
         dots: false,
-        focusOnSelect: true
+        focusOnSelect: true,
+        arrows: true,
+        autoplay: true,
+        responsive: [            
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
-
-    $(".demo").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".product-short").offset().top
-        }, 1000);
-    });
-    $(".tab-click").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".tab-top").offset().top
-        }, 1000);
-    });
-
-    // $(".dropdown").hover(            
-    //     function() {
-    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-    //         $(this).toggleClass('open');        
-    //     },
-    //     function() {
-    //         $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-    //         $(this).toggleClass('open');       
-    //     }
-    // );
-
-    $(".dropdown-2").hover(
-        function() {
-            $('.dropdown-menu-2', this).not('.in .dropdown-menu-2').stop(true, true).slideDown("400");
-            $(this).toggleClass('open');
-        },
-        function() {
-            $('.dropdown-menu-2', this).not('.in .dropdown-menu-2').stop(true, true).slideUp("400");
-            $(this).toggleClass('open');
-        }
-    );
-
-    $(".dropdown-3").hover(
-        function() {
-            $('.dropdown-menu-3', this).not('.in .dropdown-menu-3').stop(true, true).slideDown("400");
-            $(this).toggleClass('open');
-        },
-        function() {
-            $('.dropdown-menu-3', this).not('.in .dropdown-menu-3').stop(true, true).slideUp("400");
-            $(this).toggleClass('open');
-        }
-    );
-
-
 });
